96175a19057d7ad1cfa3f2214390aae485af3076
An12312620 <bhoranjali516@gmail.com>
2026-05-11 16:35:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 import streamlit as st
 import joblib
-import Pipeline
-import numpy as np
-import pandas as pd
 
+# Load Model and TF-IDF Vectorizer
 model = joblib.load('fake_job_model.pkl')
+tfidf = joblib.load('tfidf_vectorizer.pkl')
 
+# App Title
 st.title('Fake Job Posting Detection App')
 
 st.write('Enter job details below to check whether the job posting is Real or Fake.')
 
+# User Inputs
 title = st.text_input('Job Title')
 company_profile = st.text_area('Company Profile')
 description = st.text_area('Job Description')
 requirements = st.text_area('Requirements')
 benefits = st.text_area('Benefits')
 
+# Combine Input Data
 input_data = (
     title + ' ' +
     company_profile + ' ' +
@@ -24,10 +26,16 @@ input_data = (
     benefits
 )
 
+# Prediction
 if st.button('Predict'):
 
-    prediction = model.predict([input_data])[0]
+    # Convert Text into TF-IDF
+    transformed_data = tfidf.transform([input_data])
 
+    # Predict
+    prediction = model.predict(transformed_data)[0]
+
+    # Show Result
     if prediction == 1:
         st.error('This Job Posting is FAKE')
     else:
